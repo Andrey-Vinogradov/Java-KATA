f1980b73b0677edd6b01e06948e11da5a3821b75
Andrei_Vinahradau <Andrei_Vinahradau@epam.com>
2024-12-28 12:27:43 +0300

    Работает задание А, В, С, D. Everything is fine.

--- untitled/src/main/java/TestTask/TextModifier.java
+++ untitled/src/main/java/TestTask/TextModifier.java
@@ -34,7 +34,7 @@ public class TextModifier {
                 wasSpace = false;
             }
         }
-        System.out.println("после удаления двойных пробелов: "+variantA);
+        System.out.println("после удаления двойных пробелов: " + variantA);
         // b. Если в тексте присутствует знак минус (-), это значит,
         // что символ слева от этого знака надо поменять местами с символом,
         // который стоит справа от этого знака.
@@ -53,9 +53,47 @@ public class TextModifier {
                 variantB += charsB[i];
             }
         }
+        System.out.println("после обработки символа '-' : " + variantB);
+        //c. Если в тексте присутствует знак плюс (+),
+        // вам необходимо заменить его на восклицательный знак (!).
+        char[] charsC = variantB.toCharArray();
+        String variantC = "";//пустая строка для хранения результата
+
+        for (int i = 0; i < charsC.length; i++) {
+            if (charsC[i] == '+') {
+                charsC[i] = '!';
+                variantC += charsC[i];
+            } else {
+                variantC += charsC[i];
+            }
+        }
+        System.out.println("после замены символа '+' на '!' : " + variantB);
+
+        //d. В тексте могут присутствовать цифры (от 0 до 9).
+        // Вам необходимо посчитать их сумму и удалить из текста.
+        // Сумму цифр вам нужно добавить в конец результирующей строки
+        // через пробел (пробел должен стоять перед суммой цифр).
+        // Если цифр в тексте не было - "0" (ноль) в конце строки выводить не нужно.
+
+        char[] charsD = variantC.toCharArray();
+        String variantD = "";//пустая строка для хранения результата
+        int sum = 0;
+
+        for (int i = 0; i < charsD.length; i++) {
+            if (Character.isDigit(charsD[i])) {
+                sum+= Character.getNumericValue(charsD[i]);
+
+            } else {
+                variantD += charsD[i];
+            }
+        }
+        if(sum>0) {
+            variantD += " " + sum;
+        }
+        System.out.println("после подсчёта суммы цифр и их удаления: " + variantD);
 
         // Вывод результата
-        System.out.println("Откорректированная строка:");
-        System.out.println(variantB);
+        System.out.println();
+        System.out.println("Окончательный результат: " + variantD);
     }
 }
